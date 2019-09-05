0c2833d2c8c81f931f1acbd5030cc476bb86313d
Tim Thelen <tim.thelen@gmail.com>
2019-09-05 22:00:41 +0000

    remove thumb folder and project cleanup

--- BoothCam.py
+++ BoothCam.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from picamera import PiCamera
 from PIL import Image
 import os
@@ -23,7 +24,6 @@ class UsingBoothCam:
 class BoothCam:
     def __init__(self):
         self.camera = PiCamera()
-        #self.camera.resolution = (1640, 922)
         self.camera.resolution = (2592, 1944)
         self.camera.framerate = 24
         self.previewOn = False
@@ -31,20 +31,15 @@ class BoothCam:
         self.mainCanvas = self.__renderBlock(READY_PATH, (0, 0))
         self.mainCanvas.layer = 2
         print('setting up countdown')
-        self.countdownSection = self.__renderBlock('content/countdown/Blank.jpg', (0,0))
+        self.countdownSection = self.__renderBlock('content/countdown/Blank.jpg', (0, 0))
         self.countdownSection.layer = 0
         print('setting up thumbNail')
-        self.thumbNailSection = self.__renderBlock('content/thumb/new/thumb_0.jpg',(40,410))
+        self.thumbNailSection = self.__renderBlock('content/thumb/new/thumb_0.jpg', (40, 410))
         self.thumbNailSection.layer = 0
 
     def __getPaddedImage(self, imagePath):
-        # // = Floor division - division that results into whole number
-        #      adjusted to the left in the number line
         img = Image.open(imagePath)
-        pad = Image.new('RGB', (
-            ((img.size[0] + 31) // 32) * 32,
-            ((img.size[1] + 15) // 16) * 16,
-            ))
+        pad = Image.new('RGB', (self.safeResolution(img.size[0], img.size[1])))
         pad.paste(img, (0, 0))
         return pad
 
@@ -75,23 +70,14 @@ class BoothCam:
         self.countdownSection.update(pad.tobytes())
 
     def takePic(self, imgName, picNum):
-        #self.camera.resolution = (2592,1944)
         self.camera.capture(imgName)
-        #self.camera.resolution = (1515, 852)
         #os.system('mpg123 -q /home/pi/Projects/content/Click.mp3')
-        #self.showPreview()
-        self.showThumb(imgName,picNum)
+        self.showThumb(imgName, picNum)
 
-    def showThumb(self, location,picNum):
-        size = (
-            ((256 + 31) // 32) * 32,
-            ((341 + 15) // 16) * 16,
-            )
+    def showThumb(self, location, picNum):
+        size = (self.safeResolution(256, 341))
         thumb = Image.open(location)
-        #saveLoc = 'content/thumb/new/thumb_' + str(picNum) + '.jpg'
-        #os.remove(saveLoc)
-        thumb.thumbnail(size,Image.ANTIALIAS)
-        #thumb.save(saveLoc,"JPEG")
+        thumb.thumbnail(size, Image.ANTIALIAS)
         self.thumbNailSection.update(thumb.tobytes())
         self.thumbNailSection.layer = 3
 
@@ -103,7 +89,12 @@ class BoothCam:
 
     def showPreview(self):
         self.camera.start_preview(fullscreen=False,
-            window=(405,218,(800 + 31) // 32 * 32,(1066 + 15) // 16 * 16 ))
+            window=(405, 218, self.safeResolution(800, 600)))
 
     def shutdown(self):
         self.camera.close()
+
+    def safeResolution(self, x, y):
+        # // = Floor division - division that results into whole number
+        #      adjusted to the left in the number line
+        return ((x + 31) // 32 * 32, (y + 15) // 16 * 16)
--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-#import BoothCam
 import DisplayLoop
 from time import sleep
 
@@ -13,3 +12,32 @@ loopy.runSequence()
 
 sleep(3)
 print('im done')
+
+#try:
+    #target = "instructions"
+    #rendermainCanvas(target)
+    #GPIO.setmode(GPIO.BCM)
+    #GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+    #lastCheck = datetime.now()
+    #while True:
+        #input_state = GPIO.input(18)
+        #if input_state == False:
+            #runSequence()
+            #time.sleep(0.2)
+
+        #if ((datetime.now() - lastCheck).seconds > 4):
+            #if (target == "instructions"):
+                #target = "website"
+            #else:
+                #target = "instructions"
+            #rendermainCanvas(target)
+            #lastCheck = datetime.now()
+
+#except KeyboardInterrupt:
+    #pass
+    #print("keyboard press, shutting down")
+#finally:
+    #camera.close()
+#sys.exit
